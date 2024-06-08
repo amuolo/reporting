8202121332065762a5c0f1498bd4eca996dd390d
amuolo <and.muolo@gmail.com>
2024-06-08 15:14:37 +0200

    data modeling interfaces

--- Enterprise.Reporting.Utils/DataModeling.cs
+++ Enterprise.Reporting.Utils/DataModeling.cs
@@ -1,23 +1,47 @@
-﻿namespace Enterprise.Reporting.Utils;
+﻿using System.ComponentModel.DataAnnotations;
 
-public abstract record Dimension
+namespace Enterprise.Reporting.Utils;
+
+public interface IDimension
 {
+    [Key]
+    public string SystemName { get; set; }
+    public string DisplayName { get; set; }
+}
+
+public interface IHierarchicalDimension : IDimension
+{
+    public string Parent { get; set; }
+}
+
+public interface IOrderedDimension : IDimension
+{
+    public int Order { get; set; }
+}
+
+public interface IOrderedHierarchicalDimension : IHierarchicalDimension
+{
+    public int Order { get; set; }
+}
+
+public abstract record Dimension : IDimension
+{
+    [Key]
     public string SystemName { get; set; } = string.Empty;
     public string DisplayName { get; set; } = string.Empty;
 }
 
-public abstract record HierarchicalDimension : Dimension
+public abstract record HierarchicalDimension : Dimension, IDimension
 {
     public string Parent { get; set; } = string.Empty;
 }
 
-public abstract record OrderedDimension : Dimension
+public abstract record OrderedDimension : Dimension, IDimension
 {
     public int Order { get; set; }
 }
 
-public abstract record OrderedHierarchicalDimension : HierarchicalDimension
+public abstract record OrderedHierarchicalDimension : HierarchicalDimension, IHierarchicalDimension
 {
     public int Order { get; set; }
 }
-
